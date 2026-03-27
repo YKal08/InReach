f5135c2fe81278d179f27c4f3cf3f3ff9904b35f
radoykov <alex.radoykov@gmail.com>
2026-03-27 12:38:04 +0200

    Terraform ecr  changed

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -3,7 +3,8 @@ module "ecr" {
   version  = "~> 2.0"
   for_each = toset(var.ecr_repositories)
 
-  repository_name = "${var.project_name}-${each.value}"
+  repository_name                 = "${var.project_name}-${each.value}"
+  repository_image_tag_mutability = "MUTABLE"
 
   repository_lifecycle_policy = jsonencode({
     rules = [
